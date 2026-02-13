1861d053373a41886a2e50ba5ff9d5b6eee7db58
Nwt <nnttt6541@gmail.com>
2026-02-13 15:47:20 +0800

    Delete app directory

--- app/core/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from pgvector.django import VectorField
-
-# Create your models here.
-class Summary(models.Model):
-    text = models.TextField()
-    embedding = VectorField(dimensions=1024)
-    
